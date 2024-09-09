2da3854e5a91140252bc62312d324f38ac3d67f1
Tuo Nome <double.facessss@gmail.com>
2024-09-09 16:30:35 +0200

    added api categories

---
+++ src/pages/api/v1/categories.ts
@@ -0,0 +1,28 @@
+import type { APIRoute } from 'astro'
+import { supabase } from '../../../providers/supabase'
+
+export const GET: APIRoute = async () => {
+  
+  const { data, error } = await supabase
+
+    .from('areas').select(`
+        id,
+        area,
+        category,
+        sub_categories(            
+            sub_category
+        )
+    )`)   
+    .order("id", { ascending: true })
+    
+  if (error) {
+    return new Response(
+      JSON.stringify({
+        error: error.message,
+      }),
+      { status: 500 },
+    );
+  }
+
+  return new Response(JSON.stringify(data))
+}
