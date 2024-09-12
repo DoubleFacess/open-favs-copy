e6989b6fb4499bcc936040b4e9f29159abced1a1
Tuo Nome <double.facessss@gmail.com>
2024-09-12 17:01:28 +0200

    added sub_main_table insert query

--- src/pages/api/v1/main.ts
+++ src/pages/api/v1/main.ts
@@ -31,7 +31,30 @@ export const GET: APIRoute = async () => {
 }
 
 export const POST: APIRoute = async ({ request }) => {
-  const { name, title, description, url, ratings, id_provider, user_id, AI, AI_think, id_cat, tag_3, tag_4, tag_5} = await request.json();
+  
+  const { 
+    name, 
+    title, 
+    description, 
+    url, 
+    ratings, 
+    id_provider, 
+    user_id, 
+    AI, 
+    AI_think, 
+    id_cat, 
+    tag_3, 
+    tag_4, 
+    tag_5,
+    accessible, 
+    domain_exists, 
+    html_content_exists, 
+    redirect_exists, 
+    secure, 
+    status_code, 
+    valid_url
+
+  } = await request.json()
 
   // Inserisci nella tabella main_table e seleziona l'ID
   const { data: insertData, error: insertError } = await supabase
@@ -57,9 +80,7 @@ export const POST: APIRoute = async ({ request }) => {
     );
   }
   
-  const id_main = insertData ? insertData[0].id : -1
-
-  
+  const id_main = insertData ? insertData[0].id : -1  
 
   // Prepara il payload per la tabella categories_tags
   const payload = {
@@ -87,8 +108,30 @@ export const POST: APIRoute = async ({ request }) => {
         error: tagError.message,
       }),
       { status: 500 },
-    );
+    )
   }
+
   console.log(tagData)
-  return new Response(JSON.stringify(tagData), { status: 200 });
+
+  const payload_sub_main_table = {
+    id_src: id_main ? id_main : null,
+    accessible: accessible ? true : false,
+    domain_exists:  domain_exists ? true : false,
+    html_content_exists:  html_content_exists ? true : false,
+    redirect_exists:  redirect_exists ? true : false,
+    secure: secure ? true : false,
+    status_code,
+    valid_url: valid_url ? true : false 
+  }
+
+  const { data: subData, error: subError } = await supabase
+
+    .from('sub_main_table')
+    .insert(payload_sub_main_table)  
+    .select()
+
+  const msg = 'inserimento avvenuto con successo'
+
+  return new Response(JSON.stringify({'msg': msg}), { status: 200 })
+  
 }
