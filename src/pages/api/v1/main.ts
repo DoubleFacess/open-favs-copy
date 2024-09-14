3e26059d99bdb3796939d0c69eea35ed6e640827
Tuo Nome <double.facessss@gmail.com>
2024-09-14 22:35:54 +0200

    OF-8 added type and moved ai fild from main to sub

--- src/pages/api/v1/main.ts
+++ src/pages/api/v1/main.ts
@@ -44,7 +44,7 @@ export const POST: APIRoute = async ({ request }) => {
     name, 
     title, 
     description, 
-    url, 
+    url,
     ratings, 
     id_provider, 
     user_id, 
@@ -60,8 +60,8 @@ export const POST: APIRoute = async ({ request }) => {
     redirect_exists, 
     secure, 
     status_code, 
-    valid_url
-
+    valid_url,
+    type
   } = await request.json()
 
   // Inserisci nella tabella main_table e seleziona l'ID
@@ -73,8 +73,7 @@ export const POST: APIRoute = async ({ request }) => {
       description,
       url,
       id_provider,
-      user_id,
-      AI: AI ? true : false,
+      user_id,      
       ratings,
     })
     .select()
@@ -131,7 +130,9 @@ export const POST: APIRoute = async ({ request }) => {
     //redirect_exists:  redirect_exists ? true : false,
     secure: secure ? true : false,
     status_code,
-    valid_url: valid_url ? true : false 
+    valid_url: valid_url ? true : false,
+    type,
+    AI: AI ? true : false
   }
 
   const { data: subData, error: subError } = await supabase
