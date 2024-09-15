aab904fa4dc9d4cb64d9cd3c310629574c076715
Tuo Nome <double.facessss@gmail.com>
2024-09-15 12:08:00 +0200

    OF-18 edited comments in permalinks

--- src/utils/permalinks.ts
+++ src/utils/permalinks.ts
@@ -1,41 +1,74 @@
+// Importa la libreria 'slugify' per trasformare stringhe in slug leggibili
 import slugify from 'limax';
 
+// Importa le configurazioni per il sito e il blog
 import { SITE, APP_BLOG } from 'astrowind:config';
 
+// Importa una funzione utilitaria per rimuovere spazi bianchi da una stringa
 import { trim } from '~/utils/utils';
 
-export const trimSlash = (s: string) => trim(trim(s, '/'))
+/**
+ * Rimuove eventuali barre '/' all'inizio e alla fine di una stringa.
+ * @param {string} s - La stringa da elaborare.
+ * @returns {string} - La stringa senza barre iniziali e finali.
+ */
+export const trimSlash = (s: string) => trim(trim(s, '/'));
 
+/**
+ * Combina i segmenti di percorso in una singola stringa di percorso.
+ * Rimuove barre iniziali e finali extra e aggiunge una barra finale se necessario.
+ * @param {...string} params - Segmenti di percorso da combinare.
+ * @returns {string} - Il percorso combinato.
+ */
 const createPath = (...params: string[]) => {
+  console.log('create path');
+  console.log('params', params);
 
-  console.log('crete path')
-  console.log('params', params)
+  // Pulisce e combina i segmenti di percorso
   const paths = params
     .map((el) => trimSlash(el))
     .filter((el) => !!el)
-    .join('/')
-  console.log('/' + paths + (SITE.trailingSlash && paths ? '/' : ''))
-  return '/' + paths + (SITE.trailingSlash && paths ? '/' : '')
-}
+    .join('/');
 
+  // Restituisce il percorso combinato con una barra finale se necessario
+  console.log('/' + paths + (SITE.trailingSlash && paths ? '/' : ''));
+  return '/' + paths + (SITE.trailingSlash && paths ? '/' : '');
+};
+
+// Imposta il percorso base per il sito, usa '/' se non definito
 const BASE_PATHNAME = SITE.base || '/';
 
+/**
+ * Pulisce e trasforma una stringa in uno slug valido.
+ * Rimuove spazi e caratteri speciali, e separa i segmenti con '/'.
+ * @param {string} text - La stringa da trasformare in slug.
+ * @returns {string} - La stringa trasformata in uno slug.
+ */
 export const cleanSlug = (text = '') =>
   trimSlash(text)
     .split('/')
     .map((slug) => slugify(slug))
     .join('/');
 
+// Crea gli slug base per blog, categorie e tag
 export const BLOG_BASE = cleanSlug(APP_BLOG?.list?.pathname);
 export const CATEGORY_BASE = cleanSlug(APP_BLOG?.category?.pathname);
 export const TAG_BASE = cleanSlug(APP_BLOG?.tag?.pathname) || 'tag';
 
+// Crea un modello di permalink per i post del blog
 export const POST_PERMALINK_PATTERN = trimSlash(APP_BLOG?.post?.permalink || `${BLOG_BASE}/%slug%`);
 
-/** */
+/**
+ * Crea un URL canonico basato sul percorso fornito.
+ * Aggiunge o rimuove una barra finale a seconda della configurazione del sito.
+ * @param {string} [path=''] - Il percorso da convertire in URL canonico.
+ * @returns {string | URL} - L'URL canonico.
+ */
 export const getCanonical = (path = ''): string | URL => {
-  console.log('get canonical')
-  const url = String(new URL(path, SITE.site))
+  console.log('get canonical');
+  const url = String(new URL(path, SITE.site));
+
+  // Aggiusta la barra finale dell'URL in base alla configurazione del sito
   if (SITE.trailingSlash == false && path && url.endsWith('/')) {
     return url.slice(0, -1);
   } else if (SITE.trailingSlash == true && path && !url.endsWith('/')) {
@@ -44,12 +77,17 @@ export const getCanonical = (path = ''): string | URL => {
   return url;
 };
 
-/** */
+/**
+ * Genera un permalink basato sul tipo di percorso e sullo slug fornito.
+ * @param {string} [slug=''] - Lo slug da usare nel permalink.
+ * @param {string} [type='page'] - Il tipo di percorso (es. 'home', 'blog', 'asset', etc.).
+ * @returns {string} - Il permalink generato.
+ */
 export const getPermalink = (slug = '', type = 'page'): string => {
+  console.log('get permalink');
+  let permalink: string;
 
-  console.log('get permalink')
-  let permalink: string
-
+  // Restituisce direttamente gli URL assoluti o speciali
   if (
     slug.startsWith('https://') ||
     slug.startsWith('http://') ||
@@ -60,6 +98,7 @@ export const getPermalink = (slug = '', type = 'page'): string => {
     return slug;
   }
 
+  // Genera il permalink in base al tipo di percorso
   switch (type) {
     case 'home':
       permalink = getHomePermalink();
@@ -88,19 +127,35 @@ export const getPermalink = (slug = '', type = 'page'): string => {
   return definitivePermalink(permalink);
 };
 
-/** */
+/**
+ * Restituisce il permalink per la pagina principale del sito.
+ * @returns {string} - Il permalink per la home page.
+ */
 export const getHomePermalink = (): string => getPermalink('/');
 
-/** */
+/**
+ * Restituisce il permalink per la pagina "About".
+ * @returns {string} - Il permalink per la pagina "About".
+ */
 export const getAboutPermalink = (): string => getPermalink('/about');
 
-/** */
+/**
+ * Restituisce il permalink per la pagina di login.
+ * @returns {string} - Il permalink per la pagina di login.
+ */
 export const getLoginPermalink = (): string => getPermalink('/login/signin');
 
-/** */
+/**
+ * Restituisce il permalink per la pagina del blog.
+ * @returns {string} - Il permalink per la pagina del blog.
+ */
 export const getBlogPermalink = (): string => getPermalink(BLOG_BASE);
 
-/** */
+/**
+ * Crea un percorso per un asset basato sul percorso fornito.
+ * @param {string} path - Il percorso dell'asset.
+ * @returns {string} - Il percorso completo per l'asset.
+ */
 export const getAsset = (path: string): string =>
   '/' +
   [BASE_PATHNAME, path]
@@ -108,16 +163,27 @@ export const getAsset = (path: string): string =>
     .filter((el) => !!el)
     .join('/');
 
-/** */
+/**
+ * Aggiunge il percorso base al permalink generato per garantirne la completezza.
+ * @param {string} permalink - Il permalink generato.
+ * @returns {string} - Il permalink definitivo.
+ */
 const definitivePermalink = (permalink: string): string => createPath(BASE_PATHNAME, permalink);
 
-/** */
+/**
+ * Applica la funzione `getPermalink` a tutti i valori `href` in un oggetto o array.
+ * Utilizza il tipo specificato per generare i permalink corretti.
+ * @param {object} [menu={}] - L'oggetto o array di menu da elaborare.
+ * @returns {object} - L'oggetto o array con i permalink applicati.
+ */
 export const applyGetPermalinks = (menu: object = {}) => {
-  console.log('apply get permalink')
+  console.log('apply get permalink');
+  
   if (Array.isArray(menu)) {
     return menu.map((item) => applyGetPermalinks(item));
   } else if (typeof menu === 'object' && menu !== null) {
     const obj = {};
+    
     for (const key in menu) {
       if (key === 'href') {
         if (typeof menu[key] === 'string') {
@@ -137,7 +203,9 @@ export const applyGetPermalinks = (menu: object = {}) => {
         obj[key] = applyGetPermalinks(menu[key]);
       }
     }
+    
     return obj;
   }
+  
   return menu;
 };
