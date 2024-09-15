9bc165aa7c7e76ed84337e733dbdba36395dd5fd
Tuo Nome <double.facessss@gmail.com>
2024-09-15 11:58:16 +0200

    OF-18 general settings

--- src/utils/permalinks.ts
+++ src/utils/permalinks.ts
@@ -4,14 +4,19 @@ import { SITE, APP_BLOG } from 'astrowind:config';
 
 import { trim } from '~/utils/utils';
 
-export const trimSlash = (s: string) => trim(trim(s, '/'));
+export const trimSlash = (s: string) => trim(trim(s, '/'))
+
 const createPath = (...params: string[]) => {
+
+  console.log('crete path')
+  console.log('params', params)
   const paths = params
     .map((el) => trimSlash(el))
     .filter((el) => !!el)
-    .join('/');
-  return '/' + paths + (SITE.trailingSlash && paths ? '/' : '');
-};
+    .join('/')
+  console.log('/' + paths + (SITE.trailingSlash && paths ? '/' : ''))
+  return '/' + paths + (SITE.trailingSlash && paths ? '/' : '')
+}
 
 const BASE_PATHNAME = SITE.base || '/';
 
@@ -29,7 +34,8 @@ export const POST_PERMALINK_PATTERN = trimSlash(APP_BLOG?.post?.permalink || `${
 
 /** */
 export const getCanonical = (path = ''): string | URL => {
-  const url = String(new URL(path, SITE.site));
+  console.log('get canonical')
+  const url = String(new URL(path, SITE.site))
   if (SITE.trailingSlash == false && path && url.endsWith('/')) {
     return url.slice(0, -1);
   } else if (SITE.trailingSlash == true && path && !url.endsWith('/')) {
@@ -40,7 +46,9 @@ export const getCanonical = (path = ''): string | URL => {
 
 /** */
 export const getPermalink = (slug = '', type = 'page'): string => {
-  let permalink: string;
+
+  console.log('get permalink')
+  let permalink: string
 
   if (
     slug.startsWith('https://') ||
@@ -105,6 +113,7 @@ const definitivePermalink = (permalink: string): string => createPath(BASE_PATHN
 
 /** */
 export const applyGetPermalinks = (menu: object = {}) => {
+  console.log('apply get permalink')
   if (Array.isArray(menu)) {
     return menu.map((item) => applyGetPermalinks(item));
   } else if (typeof menu === 'object' && menu !== null) {
